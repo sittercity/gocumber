089edd3f688cf36953c14628be1ee3bc1640f8a1
Phil Trimble <ptrimble@sittercity.com>
2015-02-02 22:31:37 +0000

    Initial commit with draft README

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
 # gocumber
+
+Go code (golang) package that provides tools for defining and executing gherkin steps.
+
+This package uses the [go-gherkin] package by [@muhqu]. Thanks buddy!
+
+## Installation
+
+To install:
+
+```go
+go get github.com/sittercity/gocumber
+```
+
+Then import it like normal:
+
+```go
+package <yourpackage>
+
+import (
+  "github.com/sittercity/gocumber"
+)
+
+# Usage
+
+General example:
+
+```go
+func TestYourStuff(t *testing.T) {
+  steps := make(gocumber.Definitions)
+
+  steps.Given("I have an existing user", func(matches []string, _ gocumber.StepNode) {
+    // your code to create a user, you can use 'matches[1]' to pull out the
+    // name, like so: user_name := matches[1]
+  })
+}
+```
+
+Also allows for the execution of other defined steps:
+
+```go
+func TestYourStuff(t *testing.T) {
+  steps := make(gocumber.Definitions)
+
+  steps.Given(`I have an existing user with name "(.*)"`, func(matches []string, _ gocumber.StepNode) {
+    // User creation
+  })
+
+  steps.Given("I have an existing user", func([]string, gocumber.StepNode) {
+    steps.Exec(`I have an existing user with name "some name"`)
+  })
+```
+
+You can use `When` and `Then` like you would expect:
+
+```go
+  steps.When("I view all users", func([]string, gocumber.StepNode) {
+    // Do some view stuff
+  })
+
+  steps.Then("the user should be created", func([]string, gocumber.StepNode) {
+    // check that the user is created
+  })
+```
+
+If you have gherkin that includes a JSON payload you can retrieve it like so:
+
+```go
+  steps.Given("I create a user with the following data:", func(_ []string, step gocumber.StepNode) {
+    payload := step.PyString().String()
+    // Do stuff with your payload
+  })
+```
+
+Once all of your steps are defined you run them at the end of your file:
+
+```go
+steps.Run(t, "your_gherkin.feature")
+```
+
+A full example file with all of the above (with gherkin) is forthcoming.
+
+# TODO
+
+ - Need tests
+ - Should create a full example for clarity (with gherkin)
+ - Does not append gherkin background
---
+++ gocumber.go
@@ -0,0 +1,178 @@
+package gocumber
+
+import (
+	"github.com/muhqu/go-gherkin"
+	"github.com/muhqu/go-gherkin/nodes"
+	"io/ioutil"
+	"regexp"
+	"testing"
+)
+
+type Definition func([]string, StepNode)
+type Definitions map[*regexp.Regexp]Definition
+type StepNode nodes.StepNode
+type Table nodes.TableNode
+
+type matchedDefinition struct {
+	step       nodes.StepNode
+	matches    []string
+	definition Definition
+}
+
+func (matched matchedDefinition) execute() {
+	matched.definition(matched.matches, matched.step)
+}
+
+func ColumnMap(table Table) map[string]string {
+	result := make(map[string]string)
+
+	for _, row := range table.Rows() {
+		result[row[0]] = row[1]
+	}
+
+	return result
+}
+
+func RowMap(table Table) map[string]string {
+	result := make(map[string]string)
+
+	header := table.Rows()[0]
+	for _, row := range table.Rows()[1:] {
+		for i, key := range header {
+			result[key] = row[i]
+		}
+	}
+
+	return result
+}
+
+func (defs Definitions) Step(text string, def Definition) {
+	defs[regexp.MustCompile("^"+text+"$")] = def
+}
+
+func (defs Definitions) Exec(text string) (found bool) {
+	matched := defs.find(nodes.NewMutableStepNode("", text))
+	if matched == nil {
+		return false
+	}
+
+	matched.execute()
+	return true
+}
+
+func (defs Definitions) find(step nodes.StepNode) *matchedDefinition {
+	var matches []string
+	text := step.Text()
+
+	for re, definition := range defs {
+		matches = re.FindStringSubmatch(text)
+		if matches != nil {
+			return &matchedDefinition{
+				step:       step,
+				matches:    matches,
+				definition: definition,
+			}
+		}
+	}
+
+	return nil
+}
+
+func (defs Definitions) findAll(steps []nodes.StepNode) (matched []matchedDefinition, missing map[string]nodes.StepNode) {
+	missing = make(map[string]nodes.StepNode)
+
+	for _, step := range steps {
+		found := defs.find(step)
+
+		if found == nil {
+			missing[step.Text()] = step
+		} else {
+			matched = append(matched, *found)
+		}
+	}
+
+	return matched, missing
+}
+
+func (defs *Definitions) Given(text string, def Definition) { defs.Step(text, def) }
+func (defs *Definitions) When(text string, def Definition)  { defs.Step(text, def) }
+func (defs *Definitions) Then(text string, def Definition)  { defs.Step(text, def) }
+
+func (defs *Definitions) Run(t *testing.T, file string) {
+	if buffer, err := ioutil.ReadFile(file); err != nil {
+		t.Error(err.Error())
+	} else if feature, err := gherkin.ParseGherkinFeature(string(buffer)); err != nil {
+		t.Error(err.Error())
+	} else {
+		for _, scenario := range feature.Scenarios() {
+			var steps []nodes.StepNode
+
+			// TODO append Background
+
+			switch scenario := scenario.(type) {
+			case nodes.OutlineNode:
+				outlineSteps(scenario, func(step nodes.StepNode) {
+					steps = append(steps, step)
+				})
+			default:
+				for _, step := range scenario.Steps() {
+					steps = append(steps, step)
+				}
+			}
+
+			matched, missing := defs.findAll(steps)
+
+			if len(missing) == 0 {
+				for _, definition := range matched {
+					definition.execute()
+				}
+			} else {
+				for _, step := range missing {
+					t.Errorf("Undefined step:\n%s %s", step.StepType(), step.Text())
+				}
+			}
+		}
+	}
+}
+
+func outlineSteps(outline nodes.OutlineNode, callback func(nodes.StepNode)) {
+	matcher := regexp.MustCompile(`<[^>]+>`)
+	replacements := make(map[string]string)
+	replace := func(text string) string {
+		return matcher.ReplaceAllStringFunc(text, func(key string) string {
+			return replacements[key]
+		})
+	}
+
+	header := outline.Examples().Table().Rows()[0]
+	for _, row := range outline.Examples().Table().Rows()[1:] {
+		for i, key := range header {
+			replacements["<"+key+">"] = row[i]
+		}
+	}
+
+	for _, original := range outline.Steps() {
+		step := nodes.NewMutableStepNode(original.StepType(), replace(original.Text()))
+
+		if original.Table() != nil {
+			table := nodes.NewMutableTableNode()
+
+			for _, row := range original.Table().Rows() {
+				replaced := make([]string, len(row))
+
+				for i, cell := range row {
+					replaced[i] = replace(cell)
+				}
+
+				table.AddRow(replaced)
+			}
+
+			step.SetTable(table)
+		} else if original.PyString() != nil {
+			// FIXME
+			step.SetPyString(nodes.NewMutablePyStringNode())
+		}
+
+		callback(step)
+	}
+}
